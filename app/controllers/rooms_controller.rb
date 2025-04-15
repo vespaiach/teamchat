1b9b9bfc550999a9d0c5da2d86e2b80118180e3c
Trinh Nguyen <nta.toan@gmail.com>
2025-04-15 14:07:45 -0400

    show correct message type;

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,12 @@
 class RoomsController < ApplicationController
   def show
-    @room = Room.eager_load(:users, chats: :user).find(params[:id])
+    @room = Room.eager_load(:users, chats: :sender).find_by(id: params[:id])
+    @joined = @room.users.include?(current_user)
   end
 
   def index
     @all_rooms = Room.includes(:users).order(:name).all
     @joined_rooms = @all_rooms.select { |room| room.users.include?(current_user) }
-    @not_joined_rooms = @all_rooms.select { |room| room.users.exclude?(current_user) }
+    @not_joined_rooms = @all_rooms.reject { |room| room.users.include?(current_user) }
   end
 end
