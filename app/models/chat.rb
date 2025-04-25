865672ec46d24ad2f77737a300ed97057a235621
Trinh Nguyen <nta.toan@gmail.com>
2025-04-25 15:42:12 -0400

    fix room streaming

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,7 +2,7 @@ class Chat < ApplicationRecord
   belongs_to :sender, class_name: "User", foreign_key: "user_id"
   belongs_to :room
 
-  broadcasts_to ->(chat) { [ chat.room, chat.sender ] }, inserts_by: "append", partial: "chats/new_chat", target: "chats-container"
+  broadcasts_to ->(chat) { [ chat.room ] }, inserts_by: "append", partial: "chats/new_chat", target: "chats-container"
 
   has_one_attached :file_attachment
 
