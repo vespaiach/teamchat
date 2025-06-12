839b5b851b3983c030a0ad6867efe060a18ad278
Trinh Nguyen <nta.toan@gmail.com>
2025-06-11 21:59:53 -0400

    fix the loading

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -3,10 +3,7 @@
 class ChatChannel < ApplicationCable::Channel
   include LoadChats
 
-  def subscribed
-    stream_or_reject_for current_user
-  end
-
+  def subscribed; end
   def unsubscribed; end
 
   def send_chat(data)
@@ -19,13 +16,28 @@ class ChatChannel < ApplicationCable::Channel
   def load_chat_histories(data)
     return unless current_user
 
-    chats = chat_histories_by_room(data['room_id'], data['last_seen_chat_id'], data['limit'])
+    target_room = room(data)
+
+    # Pass parameters explicitly since channels don't have params
+    room_id = data['room_id']
+    last_seen_chat_id = data['last_seen_chat_id']
+    limit = data['limit'] || 30
+
+    Rails.logger.info "ChatChannel#load_chat_histories called with room_id: #{room_id}, last_seen_chat_id: #{last_seen_chat_id}, limit: #{limit}"
+
+    chats = chat_histories_by_room(room_id, last_seen_chat_id, limit)
+
+    Rails.logger.info "ChatChannel#load_chat_histories found #{chats.count} chats"
+
+    # Use Turbo::StreamsChannel to broadcast, which works with turbo_stream_from [@room]
     Turbo::StreamsChannel.broadcast_prepend_to(
-      room(data),
+      target_room,
       target: 'chats-container',
       partial: 'chats/list',
-      locals: { chats: }
+      locals: { chats: chats }
     )
+
+    Rails.logger.info 'ChatChannel#load_chat_histories broadcast completed'
   end
 
   private
